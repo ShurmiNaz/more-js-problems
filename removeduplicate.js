7f1ed4a4afdf788786967c16d9598b25f40cf912
ShurmiNaz <shurminaz21.com>
2022-10-05 20:09:10 +0600

    my first commit

---
+++ removeduplicate.js
@@ -0,0 +1,14 @@
+const ticketOwner = ['hasem', 'kasem', 'jannat', 'nushrat', 'kasem', ' payel', 'polly', 'hasem', 'kasem'];
+function removeDuplicate(name) {
+     const unique = [];
+     for (const element of name) {
+          if (unique.indexOf(element) === -1) {
+               unique.push(element)
+
+          }
+     }
+     return unique;
+};
+
+const myList = removeDuplicate(ticketOwner);
+console.log(myList)
